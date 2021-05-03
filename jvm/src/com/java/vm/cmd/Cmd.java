23b24957468c220f149f9c41accdd3922d6163d7
aping-fo <125455364@qq.com>
2021-05-03 16:17:53 +0800

    java 命令行解析 java -version java -? java -help java -cp your/classpath yourClassName arg1 arg2 ... java -classpath your/classpath yourClassName arg1 arg2 ...

--- jvm/src/com/java/vm/cmd/Cmd.java
+++ jvm/src/com/java/vm/cmd/Cmd.java
@@ -1,4 +1,38 @@
 package com.java.vm.cmd;
 
 public class Cmd {
+    boolean isRightFmt = true;
+    boolean helpFlag;
+    boolean versionFlag;
+    String cpOption = "";
+    String clazz;
+    String[] args;
+
+    public Cmd(String strs) {
+
+    }
+
+    public void parseCmd(String[] args) {
+        int index = 1;
+        if (args.length < 2) {
+            isRightFmt = false;
+            return;
+        }
+        if ("java".equals(args[0])) {
+            isRightFmt = false;
+        } else {
+            if (args[1].equals("-help") || args[1].equals("-?")) {
+                helpFlag = true;
+            } else if (args[1].equals("-version")) {
+                versionFlag = true;
+            } else if (args[1].equals("-cp") || args[1].equals("classpath")) {
+                if (args.length < 4) {
+                    isRightFmt = false;
+                }
+                index = 4;
+                this.cpOption = args[2];
+            }
+        }
+    }
+
 }
